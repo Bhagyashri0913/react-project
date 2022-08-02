dff2eacecfb45e644bad94a2d74279a296934048
Bhagyashri <Bhagyashri@LAPTOP-FN98IPH8>
2022-08-02 14:20:23 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import AppComponent from "./appComponent/AppComponent";
+
+function App() {
+  return (
+    <AppComponent />
+  );
+}
+
+export default App;
---
+++ src/appComponent/AppComponent.js
@@ -0,0 +1,16 @@
+import './appComponent.css';
+
+function AppComponent() {
+    return (
+      <div className="App">
+      <div className="App-back-ground">
+      <div className='body-box'>
+       <div className="heading">Quiz App</div>
+  
+      </div>
+      </div>
+      </div>
+    );
+  }
+  
+ export default AppComponent;
